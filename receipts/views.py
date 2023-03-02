148aaaf377a5490345670e332be33a3478d81e13
Jason Lago <jasonlago36@gmail.com>
2023-03-02 15:38:05 -0600

    Feature 8 Complete

--- receipts/views.py
+++ receipts/views.py
@@ -1,10 +1,12 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from .models import Receipt
 # Create your views here.
 
 
+@login_required
 def receipt_list(request):
-    receipt = Receipt.objects.all()
+    receipt = Receipt.objects.filter(purchaser=request.user)
     context = {
         "receipt_object": receipt,
     }
